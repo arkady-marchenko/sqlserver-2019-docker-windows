332437f6685b5512126b849329ac425b20156c5b
Arkady Marchenko <arkady.marchenko@inspire-tech.com>
2021-03-30 17:33:06 +0700

    add script to extract sql server data folder

---
+++ extract-sql2019-data.ps1
@@ -0,0 +1,3 @@
+docker container create inspiretech/sqlserver-2019:ltsc2019-210328.1
+docker container ls
+docker cp <container_id>:C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA .\
